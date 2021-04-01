3562873082f7ef26a4f5410c7faa22dbf4b5b7f7
Asyno <47446626+Asynoo@users.noreply.github.com>
2021-04-02 00:20:12 +0200

    Changed from Realtime Database to Document Database for actual funtionality. Complete code refractor and cleanup. Fully Functional though.

--- app/src/main/java/com/example/fishycodex/AddMenuActivity.java
+++ app/src/main/java/com/example/fishycodex/AddMenuActivity.java
@@ -1,29 +1,20 @@
 package com.example.fishycodex;
 
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.fishycodex.MainActivity;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import android.app.Activity;
+import com.google.firebase.firestore.FirebaseFirestore;
 import android.content.Intent;
 import android.os.Bundle;
-import android.preference.EditTextPreference;
-import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.os.Bundle;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class AddMenuActivity extends AppCompatActivity {
 
-    FirebaseDatabase database = FirebaseDatabase.getInstance("https://fishy-codex-default-rtdb.europe-west1.firebasedatabase.app/");
-
-
+    FirebaseFirestore database = FirebaseFirestore.getInstance();
     Button AddButton;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,48 +23,67 @@ public class AddMenuActivity extends AppCompatActivity {
         addNewCatch();
     }
 
-    public void addNewCatch(){
+    public void addNewCatch() {
         AddButton = (Button) findViewById(R.id.addCatchID);
         AddButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 fishDataWrite();
-                Intent loadAddPage = new Intent(AddMenuActivity.this,MainActivity.class);
+                Intent loadAddPage = new Intent(AddMenuActivity.this, MainActivity.class);
                 startActivity(loadAddPage);
             }
         });
     }
 
-    public void fishDataWrite(){
-        speciesMethod();
-        locationMethod();
-        sizeMethod();
-        dateMethod();
+    public void fishDataWrite() {
+        Map<String, Object> Fish = new HashMap<>();
+
+        EditText speciesValue = (EditText) findViewById(R.id.speciesID);
+        Fish.put("Species", speciesValue.getText().toString());
+
+        EditText locationValue = (EditText) findViewById(R.id.locationID);
+        Fish.put("Location", locationValue.getText().toString());
+
+        EditText sizeValue = (EditText) findViewById(R.id.sizeID);
+        Fish.put("Size", sizeValue.getText().toString());
+
+        EditText dateValue = (EditText) findViewById(R.id.dateID);
+        Fish.put("Date", dateValue.getText().toString());
+
+        database.collection("Fish").add(Fish);
     }
 
-    private void speciesMethod(){
-        EditText speciesValue = (EditText)findViewById(R.id.speciesID);
-        DatabaseReference speciesFish = database.getReference("Species");
-        speciesFish.setValue(speciesValue.getText().toString());
+    /*
+    public void speciesMethod() {
+        EditText speciesValue = (EditText) findViewById(R.id.speciesID);
+        Map<String, Object> Fish = new HashMap<>();
+        Fish.put("Species", speciesValue.getText().toString());
+        database.collection("Fish").add(Fish);
     }
 
-    private void locationMethod(){
-        EditText locationValue = (EditText)findViewById(R.id.locationID);
-        DatabaseReference locationFish = database.getReference("Location");
-        locationFish.setValue(locationValue.getText().toString());
+    public void locationMethod() {
+        EditText locationValue = (EditText) findViewById(R.id.locationID);
+        Map<String, Object> Fish = new HashMap<>();
+        Fish.put("Location", locationValue.getText().toString());
+        database.collection("Fish").add(Fish);
+
+
     }
 
-    private void sizeMethod(){
-        EditText sizeValue = (EditText)findViewById(R.id.sizeID);
-        DatabaseReference sizeFish = database.getReference("Size");
-        sizeFish.setValue(sizeValue.getText().toString());
+    public void sizeMethod() {
+        EditText sizeValue = (EditText) findViewById(R.id.sizeID);
+        Map<String, Object> Fish = new HashMap<>();
+        Fish.put("Size", sizeValue.getText().toString());
+        database.collection("Fish").add(Fish);
     }
 
-    private void dateMethod(){
-        EditText dateValue = (EditText)findViewById(R.id.dateID);
-        DatabaseReference dateFish = database.getReference("Date");
-        dateFish.setValue(dateValue.getText().toString());
+    public void dateMethod() {
+        EditText dateValue = (EditText) findViewById(R.id.dateID);
+        Map<String, Object> Fish = new HashMap<>();
+        Fish.put("Date", dateValue.getText().toString());
+        database.collection("Fish").add(Fish);
     }
+    */
 
 }
--- app/src/main/java/com/example/fishycodex/MainActivity.java
+++ app/src/main/java/com/example/fishycodex/MainActivity.java
@@ -1,124 +1,67 @@
 package com.example.fishycodex;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
+import androidx.annotation.NonNull;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import static android.content.ContentValues.TAG;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
-    //FirebaseDatabase database = FirebaseDatabase.getInstance("https://fishy-codex-default-rtdb.europe-west1.firebasedatabase.app/");
-    ListView fishView;
+    FirebaseFirestore database = FirebaseFirestore.getInstance();
     Button Addbutton;
-    //EditText GetValue;
-    String[] ListElements = new String[] {
+    ArrayList<String> listItems=new ArrayList<String>();
+    ArrayAdapter<String> adapter;
 
-    };
-/*
-    public void database(){
-        DatabaseReference speciesFish = database.getReference("Species");
-        speciesFish.setValue("Rainbow Trout");
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_main);
+        adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+        setListAdapter(adapter);
+        loadAddMenu();
+        readDatabase();
+    }
 
-        DatabaseReference locationFish = database.getReference("Location");
-        locationFish.setValue("Henne Put and Take");
-
-        DatabaseReference sizeFish = database.getReference("Size");
-        sizeFish.setValue("3kg");
-
-        DatabaseReference dateFish = database.getReference("Date");
-        dateFish.setValue("15.3.2021");
-
-
-        locationFish.addValueEventListener(new ValueEventListener() {
+    public void readDatabase(){
+        database.collection("Fish").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                Log.w(TAG, "Failed to read value.", error.toException());
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        //listItems.add(document.getId() + document.getData());
+                        listItems.add(String.valueOf(document.getData()));
+                        Log.d(TAG, document.getId() + document.getData());
+                    }
+                } else {
+                    Log.w(TAG, "Error getting documents.", task.getException());
+                }
             }
         });
     }
-    */
 
-    public void init(){
-        fishView = (ListView) findViewById(R.id.fishListView);
-
-        final List< String > ListElementsArrayList = new ArrayList < String >
-                (Arrays.asList(ListElements));
-
-
-        final ArrayAdapter < String > adapter = new ArrayAdapter < String >
-                (MainActivity.this, android.R.layout.simple_list_item_1,
-                        ListElementsArrayList);
+    public void loadAddMenu(){
         Addbutton = (Button) findViewById(R.id.openNewFishView);
         Addbutton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 Intent loadAddPage = new Intent(MainActivity.this,AddMenuActivity.class);
-                startActivity(loadAddPage);
-            }
-        });
-
+                startActivity(loadAddPage);}});
     }
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        init();
+    public void reloadListView(View v){
+        adapter.notifyDataSetChanged();
+        readDatabase();
     }
 }
-
-/*
-        listview = (ListView) findViewById(R.id.listView1);
-        Addbutton = (Button) findViewById(R.id.button1);
-        GetValue = (EditText) findViewById(R.id.editText1);
-
-        final List< String > ListElementsArrayList = new ArrayList < String >
-                (Arrays.asList(ListElements));
-
-
-        final ArrayAdapter < String > adapter = new ArrayAdapter < String >
-                (MainActivity.this, android.R.layout.simple_list_item_1,
-                        ListElementsArrayList);
-
-        listview.setAdapter(adapter);
-
-        Addbutton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                //DatabaseReference speciesFish = database.getReference("Species");
-                //ListElementsArrayList.add(GetValue.getText().toString());
-                //speciesFish.setValue(GetValue.getText().toString());
-                //adapter.notifyDataSetChanged();
-
-            }
-        });
- */
