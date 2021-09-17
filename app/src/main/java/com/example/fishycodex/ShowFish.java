087839344e6eae60d1208001cdf97c96d85390d2
Tegridy Code <marcgabel@live.dk>
2021-09-17 19:15:34 +0200

    Improved List View Code

--- app/src/main/java/com/example/fishycodex/ShowFish.java
+++ app/src/main/java/com/example/fishycodex/ShowFish.java
@@ -1,39 +1,31 @@
 package com.example.fishycodex;
 
-import androidx.annotation.NonNull;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ShowFish extends ListActivity {
 
     FirebaseFirestore database = FirebaseFirestore.getInstance();
-    ArrayList<String> listItems=new ArrayList<String>();
+    ArrayList<String> listItems= new ArrayList<>();
     ArrayAdapter<String> adapter;
-
-    private Button reloadButton;
-    private TextView yourFishyCodexBanner;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.activity_show_fish);
-        adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+        adapter= new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listItems);
         setListAdapter(adapter);
         readDatabase();
         loadAddMenu();
@@ -43,39 +35,30 @@ public class ShowFish extends ListActivity {
 
     public void readDatabase(){
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        String uid = user.getUid();
-        database.collection(uid).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    for (QueryDocumentSnapshot document : task.getResult()) {
-                        listItems.add("\n" + "Species: " + document.getString("Species") + "\n" + "Location: " + document.getString("Location") + "\n" + "Size: " + document.getString("Size") + "\n" + "Date: " + document.getString("Date") + "\n");
-                    }
+        String uid = user != null ? user.getUid() : null;
+        assert uid != null;
+        database.collection(uid).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                    listItems.add("\n" + "Species: " + document.getString("Species") + "\n" + "Location: " + document.getString("Location") + "\n" + "Size: " + document.getString("Size") + "\n" + "Date: " + document.getString("Date") + "\n");
                 }
             }
         });
     }
 
     public void loadAddMenu(){
-        reloadButton = findViewById(R.id.reloadListView);
-        reloadButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                adapter.notifyDataSetChanged();
-                readDatabase();
-            }
+        Button reloadButton = findViewById(R.id.reloadListView);
+        reloadButton.setOnClickListener(v -> {
+            adapter.notifyDataSetChanged();
+            readDatabase();
         });
     }
 
     public void goBack(){
-        yourFishyCodexBanner = findViewById(R.id.yourFishyCodexBanner);
-        yourFishyCodexBanner.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Intent loadAddPage = new Intent(ShowFish.this, Home.class);
-                startActivity(loadAddPage);
-            }
+        TextView yourFishyCodexBanner = findViewById(R.id.yourFishyCodexBanner);
+        yourFishyCodexBanner.setOnClickListener(v -> {
+            Intent loadAddPage = new Intent(ShowFish.this, Home.class);
+            startActivity(loadAddPage);
         });
     }
 
