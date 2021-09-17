33354e98c80e867d5a766815eede6c8a166e9357
Tegridy Code <marcgabel@live.dk>
2021-09-17 19:07:32 +0200

    Improved Home View Code

--- app/src/main/java/com/example/fishycodex/Home.java
+++ app/src/main/java/com/example/fishycodex/Home.java
@@ -1,15 +1,13 @@
 package com.example.fishycodex;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Home extends AppCompatActivity {
 
-    private Button loadAdd, loadFish;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,20 +17,16 @@ public class Home extends AppCompatActivity {
     }
 
     public void loadAddMenu(){
-        loadAdd = (Button) findViewById(R.id.openNewFishView);
-        loadAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent loadAddPage = new Intent(Home.this,AddMenuActivity.class);
-                startActivity(loadAddPage);}});
+        Button loadAdd = findViewById(R.id.openNewFishView);
+        loadAdd.setOnClickListener(v -> {
+            Intent loadAddPage = new Intent(Home.this,AddMenuActivity.class);
+            startActivity(loadAddPage);});
     }
 
     public void loadFishMenu(){
-        loadFish = (Button) findViewById(R.id.openFishView);
-        loadFish.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent loadAddPage = new Intent(Home.this,ShowFish.class);
-                startActivity(loadAddPage);}});
+        Button loadFish = findViewById(R.id.openFishView);
+        loadFish.setOnClickListener(v -> {
+            Intent loadAddPage = new Intent(Home.this,ShowFish.class);
+            startActivity(loadAddPage);});
     }
 }
